12958b38662eabc9c9d99bd717e7ef4e88864c17
abab <ababup1192@gmail.com>
2019-01-29 23:00:28 +0900

    [GREEN] 2点交叉モード導入

--- src/Main.elm
+++ src/Main.elm
@@ -16,7 +16,7 @@ module Main exposing
 
 import Browser
 import Html exposing (..)
-import Html.Attributes exposing (..)
+import Html.Attributes exposing (class, type_, value)
 import Html.Events exposing (..)
 import Random
 
@@ -68,24 +68,59 @@ type CrossingMode
     | TwoPointCrossing Index Index
 
 
+mode2Text : CrossingMode -> String
+mode2Text crossingMode =
+    case crossingMode of
+        Crossing _ ->
+            "単交叉"
+
+        TwoPointCrossing _ _ ->
+            "2点交叉"
+
+
 type alias Model =
     { geneA : Gene, geneB : Gene, crossingMode : CrossingMode, generation : Int }
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
+    let
+        defaultCrossingMode =
+            Crossing -1
+    in
     ( { geneA = [ False, False, False, False, False, False, False, False, False, False ]
       , geneB = [ False, False, False, False, False, False, False, False, False, False ]
-      , crossingMode = Crossing 0
+      , crossingMode = defaultCrossingMode
       , generation = 1
       }
-    , genCrossingSeparator (Crossing 0)
+    , genCrossingSeparator defaultCrossingMode
     )
 
 
 genCrossingSeparator : CrossingMode -> Cmd Msg
 genCrossingSeparator crossingMode =
-    Random.generate GenCrossingSeparator <| (Random.int 1 8 |> Random.map Crossing)
+    case crossingMode of
+        Crossing _ ->
+            Random.generate GenCrossingSeparator <| (Random.int 1 8 |> Random.map Crossing)
+
+        TwoPointCrossing _ _ ->
+            Random.generate GenCrossingSeparator <|
+                (Random.int 0 9
+                    |> Random.andThen
+                        (\x ->
+                            let
+                                list =
+                                    List.range 0 9 |> List.filter (\n -> n /= x)
+
+                                h =
+                                    List.head list |> Maybe.withDefault -1
+
+                                t =
+                                    List.drop 1 list
+                            in
+                            Random.uniform h t |> Random.map (\y -> TwoPointCrossing (min x y) (max x y))
+                        )
+                )
 
 
 
@@ -102,6 +137,7 @@ type Msg
     = SwapGene Individual Index
     | GenerationalChange
     | GenCrossingSeparator CrossingMode
+    | SwapMode
 
 
 swapGene : Index -> Gene -> Gene
@@ -125,10 +161,10 @@ update msg ({ geneA, geneB, crossingMode, generation } as model) =
         SwapGene individual index ->
             case individual of
                 A ->
-                    ( { model | geneA = swapGene index geneA }, genCrossingSeparator (Crossing 0) )
+                    ( { model | geneA = swapGene index geneA }, genCrossingSeparator crossingMode )
 
                 B ->
-                    ( { model | geneB = swapGene index geneB }, genCrossingSeparator (Crossing 0) )
+                    ( { model | geneB = swapGene index geneB }, genCrossingSeparator crossingMode )
 
         GenerationalChange ->
             let
@@ -140,10 +176,22 @@ update msg ({ geneA, geneB, crossingMode, generation } as model) =
                         TwoPointCrossing idx1 idx2 ->
                             twoPointCrossing idx1 idx2 geneA geneB
             in
-            ( { model | geneA = nextGeneA, geneB = nextGeneB, generation = generation + 1 }, genCrossingSeparator (Crossing 0) )
+            ( { model | geneA = nextGeneA, geneB = nextGeneB, generation = generation + 1 }, genCrossingSeparator crossingMode )
 
         GenCrossingSeparator crgMode ->
             ( { model | crossingMode = crgMode }, Cmd.none )
+
+        SwapMode ->
+            let
+                nextMode =
+                    case crossingMode of
+                        Crossing _ ->
+                            TwoPointCrossing -1 -1
+
+                        TwoPointCrossing _ _ ->
+                            Crossing -1
+            in
+            ( { model | crossingMode = nextMode, generation = 1 }, genCrossingSeparator nextMode )
 
 
 
@@ -205,8 +253,46 @@ afterIndivisualView crossingMode gene =
                     ]
                 ]
 
-        TwoPointCrossing index index2 ->
-            ul [] []
+        TwoPointCrossing start end ->
+            let
+                baseStart =
+                    List.take start gene
+
+                target =
+                    gene |> List.drop start |> List.take (end - start)
+
+                baseEnd =
+                    List.drop end gene
+            in
+            ul []
+                [ li [ class "chunk-base" ]
+                    [ ul [] <|
+                        (baseStart
+                            |> List.map
+                                (\g ->
+                                    li [] [ text <| bool2Text g ]
+                                )
+                        )
+                    ]
+                , li [ class "chunk-target" ]
+                    [ ul [] <|
+                        (target
+                            |> List.map
+                                (\g ->
+                                    li [] [ text <| bool2Text g ]
+                                )
+                        )
+                    ]
+                , li [ class "chunk-base" ]
+                    [ ul [] <|
+                        (baseEnd
+                            |> List.map
+                                (\g ->
+                                    li [] [ text <| bool2Text g ]
+                                )
+                        )
+                    ]
+                ]
 
 
 view : Model -> Html Msg
@@ -234,6 +320,7 @@ view { geneA, geneB, crossingMode, generation } =
             ]
         , article []
             [ input [ type_ "button", value "世代交代", class "pure-button pure-button-primary", onClick GenerationalChange ] []
+            , input [ type_ "button", value <| mode2Text crossingMode, class "pure-button pure-button-primary", onClick SwapMode ] []
             ]
         , article [ class "after" ]
             [ h1 [] [ text <| "第" ++ String.fromInt (generation + 1) ++ "世代" ]
